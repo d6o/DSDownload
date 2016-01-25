1c4c923cb48e8df1617fb0e05906ffb1729417e0
Diego Martins de Siqueira <dieg0@live.com>
2016-01-25 07:07:45 -0200

    Adding package to PiPy

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mdownload',
+  packages = ['mdownload'], # this must be the same as the name above
+  version = '0.1',
+  description = 'It is a small library to multi-threaded downloads.',
+  author = 'Diego Siqueira',
+  author_email = 'dieg0@live.com',
+  url = 'https://github.com/DiSiqueira/mdownload', # use the URL to the github repo
+  download_url = 'https://github.com/DiSiqueira/mdownload/tarball/0.1', # I'll explain this in a second
+  keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'], # arbitrary keywords
+  classifiers = [],
+)
