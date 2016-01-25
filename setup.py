a586600ee01fe3b227a5bf724cdef1c03d4c82a5
Diego Martins de Siqueira <dieg0@live.com>
2016-01-25 07:51:52 -0200

    Tag 0.6

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mdownload',
   packages = ['mdownload'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.6',
   description = 'It is a small library to multi-threaded downloads.',
   author = 'Diego Siqueira',
   author_email = 'dieg0@live.com',
   url = 'https://github.com/DiSiqueira/mdownload', # use the URL to the github repo
-  download_url = 'https://github.com/DiSiqueira/mdownload/tarball/0.4', # I'll explain this in a second
+  download_url = 'https://github.com/DiSiqueira/mdownload/tarball/0.6', # I'll explain this in a second
   keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'], # arbitrary keywords
   classifiers = [],
+  license='MIT',
 )
