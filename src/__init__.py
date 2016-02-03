ded9e2d868f5d54db1e1334b971415a9dd5dda85
Diego Martins de Siqueira <dieg0@live.com>
2016-02-03 08:54:02 -0200

    Windows Change

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ def main(argv=sys.argv[0]):
     description =   "DSDownload is a fully featured download library with focus on performance"
     parser = argparse.ArgumentParser(
         description = description)
-    parser.add_argument("--version", action="version", version='1.6.0.0',
+    parser.add_argument("--version", action="version", version='1.6.0.1',
         help="Version Info")
     parser.add_argument("--workers", type=int, default=5, 
         help="Number of parallel downloads. The default is 5.")
@@ -31,6 +31,9 @@ def main(argv=sys.argv[0]):
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
+    download = DSDownload
+    if type(sys) is type(DSDownload):
+        download = DSDownload.DSDownload
     try:
         DSDownload(args.urls, args.workers, args.output)
         print('All files were downloaded.')
