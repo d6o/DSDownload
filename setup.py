5a0101e14f4bf7c2ee734f09d63a7978c4c79412
Diego Martins de Siqueira <dieg0@live.com>
2016-03-11 00:52:18 -0300

    Reformating Setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-
 from setuptools import setup
 
 setup(
-  name = 'DSDownload',
-  version = '1.6.0.2',
-  description = 'Program and module for download queue optimization using multi-thread.',
-  url = 'https://github.com/DiSiqueira/DSDownload',
-  author = 'Diego Siqueira',
-  author_email = 'dieg0@live.com',
-  license = 'MIT',
-  package_dir = { 'DSDownload' : 'src' },
-  packages = [ 'DSDownload' ],
-  zip_safe = False, 
-  keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'],
-  entry_points = 
-  {
-      'console_scripts': 
-      [
-          'dsdownload = DSDownload:main',
-      ],
-  },
+    name='DSDownload',
+    version='1.6.0.2',
+    description='Program and module for download queue optimization using multi-thread.',
+    url='https://github.com/DiSiqueira/DSDownload',
+    author='Diego Siqueira',
+    author_email='dieg0@live.com',
+    license='MIT',
+    package_dir={'DSDownload': 'src'},
+    packages=['DSDownload'],
+    zip_safe=False,
+    keywords=['download', 'thread', 'speed', 'resume', 'multi', 'simple'],
+    entry_points=
+    {
+        'console_scripts':
+            [
+                'dsdownload = DSDownload:main',
+            ],
+    },
 )
